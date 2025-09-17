9f4d68998719a54afb5cade6a42723106ce1fd45
Samanta Ortuño Miquel <102739926+Samantao93@users.noreply.github.com>
2025-09-17 18:55:40 +0200

    Update selection_process.R

--- selection_process.R
+++ selection_process.R
@@ -1,37 +1,33 @@
+# Set working directory
 setwd("path/busqueda")
+
+# Load libraries
 library(readxl)
 library(dplyr)
 
+# Load data
 pubmed <- read.csv("./csv-Lungcancer-set.csv")
 scopus <- read.csv("./scopus.csv")
 wos <- read_excel("./savedrecs.xls")
 
+# Keep only the columns we need and standardize names
 pubmed2<-select(pubmed,PMID,DOI,Title,Authors) %>%
   mutate(Document.Type=NA,database2='PUBMED')
-# table(is.na(pubmed2$DOI))
 
 scopus2<-select(scopus,PMID=PubMed.ID,DOI,Title,Authors,Document.Type) %>%
   mutate(database2='SCOPUS')
-# table(is.na(scopus2$PMID_scopus))
 
 wos2<-select(wos,PMID="Pubmed Id",DOI,Title="Article Title",Authors,Document.Type="Document Type") %>%
   mutate(database2='WOS')
-# table(is.na(wos2$DOI))
+
+# Fix obvious DOI issues
 wos2$DOI[wos2$DOI=='10.1016/j.ebiom2202.104143']<-'10.1016/j.ebiom.2022.104143'
 wos2$DOI[is.na(wos2$DOI)]<-c(1,2,3,4)
 
-final<-rbind(pubmed2,scopus2,wos2)
-
-counts<- final %>% group_by(DOI) %>% reframe(total=n())
-
-table(counts$total)
-
-# final2<-final[!duplicated(final$DOI),]
-
+# Combine sources on DOI
 final_join<-full_join(wos2,scopus2,by=c("DOI"))
 final_join<-full_join(final_join,pubmed2,by="DOI")
 write.table(final_join,"./total_paper_all_db.csv",na="",row.names = F,sep=";")
-
 
 doi_w<-data.frame(DOI=wos2$DOI)
 doi_s<-data.frame(DOI=scopus2$DOI)
@@ -41,9 +37,11 @@ doi<-rbind(doi_w,doi_p,doi_s)
 doi<-filter(doi,!is.na(DOI))
 write.table(doi,"DOI_total.csv",row.names = F,na='',sep=';')
 
+# Detect duplicated / triplicated DOIs 
 final<-doi %>% group_by(DOI) %>% summarise(total=n())
 sum(final>1)
 
+# Filter by Document Type
 all<-final_join
 all$Title<- ifelse(!is.na(all$`Title.x`), all$`Title.x`, ifelse(!is.na(all$`Title.y`), all$`Title.y`,all$`Title`))
 all$Author<- ifelse(!is.na(all$Authors.x), all$Authors.x, ifelse(!is.na(all$Authors.y), all$Authors.y,all$Authors))
@@ -56,5 +54,3 @@ write.table(filtrado_bueno,"./filtrado_bueno.csv",na="",row.names = F,sep=";")
 
 filtrado_nobueno<-filter(all[!all$DOI %in% filtrado_bueno$DOI,])
 write.table(filtrado_nobueno,"./filtrado_nobueno.csv",na="",row.names = F,sep=";")
-
-
